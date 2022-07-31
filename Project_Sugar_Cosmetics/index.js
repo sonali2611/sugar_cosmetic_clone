7f0213806547d7da11b6d72e781f37b5c9cabdc1
sonali shirke <sonali2611shirke@gmail.com>
2022-07-31 18:11:29 +0530

    successfully added the sugar website

---
+++ Project_Sugar_Cosmetics/index.js
@@ -0,0 +1,78 @@
+
+var addToCardArr = JSON.parse(localStorage.getItem("addToCardData")) || []
+
+
+function bestSellerCardOne(){
+    const productImage = document.querySelector('#card1_row1 img').getAttribute("src");
+    const productName = document.querySelector('#card1_row1 p').textContent;
+    const productCurrency = document.querySelector('#card1_row1 span').textContent;
+    const productPrice = document.querySelector('#card1_row1 span+span').textContent;
+    console.log(productImage, productName, productCurrency, productPrice)
+
+    let addToCardObject = {
+        img: productImage
+        ,name: productName,
+        ProductCurrency: productCurrency,
+        price: productPrice,
+    }
+    addToCardArr.push(addToCardObject);
+    localStorage.setItem("addToCardData", JSON.stringify(addToCardArr))
+    window.location.reload();
+}
+
+function superSaverCart1(){
+    const productImage = document.querySelector('#card1_row2 img').getAttribute("src");
+    const productName = document.querySelector('#card1_row2 p').textContent;
+    const productCurrency = document.querySelector('#card1_row2 span+span+span').textContent;
+    const productPrice = document.querySelector('#card1_row2 span+span+span+span').textContent;
+    console.log(productImage, productName, productCurrency, productPrice)
+
+    let addToCardObject = {
+        img: productImage
+        ,name: productName,
+        ProductCurrency: productCurrency,
+        price: productPrice,
+    }
+    addToCardArr.push(addToCardObject);
+    localStorage.setItem("addToCardData", JSON.stringify(addToCardArr))
+    window.location.reload();
+
+}
+
+function superSaverCart2(){
+    const productImage = document.querySelector('#card2_row2 img').getAttribute("src");
+    const productName = document.querySelector('#card2_row2 p').textContent;
+    const productCurrency = document.querySelector('#card2_row2 span+span+span').textContent;
+    const productPrice = document.querySelector('#card2_row2 span+span+span+span').textContent;
+    console.log(productImage, productName, productCurrency, productPrice)
+
+    let addToCardObject = {
+        img: productImage
+        ,name: productName,
+        ProductCurrency: productCurrency,
+        price: productPrice,
+    }
+    addToCardArr.push(addToCardObject);
+    localStorage.setItem("addToCardData", JSON.stringify(addToCardArr))
+    window.location.reload();
+
+}
+
+function superSaverCart3(){
+    const productImage = document.querySelector('#card1_row3 img').getAttribute("src");
+    const productName = document.querySelector('#card1_row3 p').textContent;
+    const productCurrency = document.querySelector('#card1_row3 span+span+span').textContent;
+    const productPrice = document.querySelector('#card1_row3 span+span+span+span').textContent;
+    console.log(productImage, productName, productCurrency, productPrice)
+
+    let addToCardObject = {
+        img: productImage
+        ,name: productName,
+        ProductCurrency: productCurrency,
+        price: productPrice,
+    }
+    addToCardArr.push(addToCardObject);
+    localStorage.setItem("addToCardData", JSON.stringify(addToCardArr))
+    window.location.reload();
+
+}
